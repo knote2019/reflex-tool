d5968e3c38bfe59ce307732643a050c7c890c1b0
KennyKang <227105027+KennyKangNV2025@users.noreply.github.com>
2025-10-29 16:13:11 +0800

    fix

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="reflex-tool",
+    favicon_path="NVIDIA-logo-white-16x9.png",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
