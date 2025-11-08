571f60e616bdffd61520769d64574ea79af03457
KennyKang <227105027+KennyKangNV2025@users.noreply.github.com>
2025-11-08 23:04:35 +0800

    fix

--- rxconfig.py
+++ rxconfig.py
@@ -6,5 +6,9 @@ config = rx.Config(
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
-    ]
+    ],
+    backend_host="0.0.0.0",
+    backend_port=8000,
+    frontend_host="0.0.0.0",
+    frontend_port=3000,
 )
