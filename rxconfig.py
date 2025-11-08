c78130a9e693b6c34438dae0957a1dd6cf0050ca
KennyKang <227105027+KennyKangNV2025@users.noreply.github.com>
2025-11-08 23:05:27 +0800

    fix

--- rxconfig.py
+++ rxconfig.py
@@ -3,10 +3,10 @@ import reflex as rx
 config = rx.Config(
     app_name="reflex_tool",
     favicon_path="NVIDIA-logo-white-16x9.png",
+    frontend_host="0.0.0.0",
+    frontend_port=3000,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ],
-    frontend_host="0.0.0.0",
-    frontend_port=3000,
 )
