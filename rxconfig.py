2dbf584d83d83af59c7b5b7d8e48b311a458bfbc
KennyKang <227105027+KennyKangNV2025@users.noreply.github.com>
2025-11-09 00:59:25 +0800

    add nginx

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,9 @@ config = rx.Config(
     app_name="reflex_tool",
     favicon_path="NVIDIA-logo-white-16x9.png",
     frontend_host="0.0.0.0",
-    frontend_port=3000,
+    frontend_port=6000,
+    backend_host="0.0.0.0",
+    backend_port=7000,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
