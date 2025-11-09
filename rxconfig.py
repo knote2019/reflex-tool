0cdb1667ad225ca88eb61a8552c5385d7c3e7e86
KennyKang <227105027+KennyKangNV2025@users.noreply.github.com>
2025-11-09 14:25:26 +0800

    init

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="reflex_tool",
     favicon_path="NVIDIA-logo-white-16x9.png",
+    frontend_port=8080,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
